5f27c4419119ac5ecde9ef5e88f22ef0db7687e0
KushagraGitter <kushagra305@gmail.com>
2022-09-18 10:13:46 +0530

    1 file modified

--- src/components/TopStories.jsx
+++ src/components/TopStories.jsx
@@ -1,16 +1,13 @@
 import React, { useState, useEffect, Suspense, lazy } from 'react';
-
-const Story = lazy(() => import('./Story'));
+import Story from './Story';
 
 const TopStories = ({ topStories }) => {
   return (
     <div>
       <h1>Top Stories</h1>
-      <Suspense fallback={<p id="loading">Loading...</p>}>
-        {topStories.map((storyId) => {
-          return <Story key={storyId} storyId={storyId} />;
-        })}
-      </Suspense>
+      {topStories.map((storyId) => {
+        return <Story key={storyId} storyId={storyId} />;
+      })}
     </div>
   );
 };
